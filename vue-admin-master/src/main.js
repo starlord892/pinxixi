11d448488ae92725bb4b79e48ba12771019071ad
黄海 <280904958@qq.com>
2019-05-16 23:01:49 +0800

    商品模块

--- vue-admin-master/src/main.js
+++ vue-admin-master/src/main.js
@@ -2,7 +2,7 @@ import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 //import './assets/theme/theme-green/index.css'
 import VueRouter from 'vue-router'
 import store from './vuex/store'
@@ -27,20 +27,20 @@ Vue.prototype.$http = axios
 //NProgress.configure({ showSpinner: false });
 
 const router = new VueRouter({
-    routes
+  routes
 })
 
 router.beforeEach((to, from, next) => {
-    //NProgress.start();
-    if (to.path == '/login') {
-        sessionStorage.removeItem('user');
-    }
-    let user = JSON.parse(sessionStorage.getItem('user'));
-    if (!user && to.path != '/login') {
-        next({ path: '/login' })
-    } else {
-        next()
-    }
+  //NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
 })
 
 //router.afterEach(transition => {
@@ -48,11 +48,11 @@ router.beforeEach((to, from, next) => {
 //});
 
 new Vue({
-    //el: '#app',
-    //template: '<App/>',
-    router,
-    store,
-    //components: { App }
-    render: h => h(App)
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
 }).$mount('#app')
 
